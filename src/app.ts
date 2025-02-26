a5f687dbd7b6b3ebe750fa2956e51c299054494b
guochao.song <soddygo@qq.com>
2025-02-26 15:03:45 +0800

    response 拦截器细节问题修复

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,25 @@ import { message } from 'antd';
 export const request: any = {
   timeout: 10000,
   headers: { 'X-Requested-With': 'XMLHttpRequest' },
+  // 错误处理： umi@3 的错误处理方案。
+  errorConfig: {
+    // 错误抛出
+    errorThrower: (res) => {
+      const { success, displayCode, message } = res;
+      if (!success) {
+        const error: any = new Error(message);
+        error.displayCode = displayCode;
+        error.message = message;
+        throw error; // 抛出自制的错误
+      }
+    },
 
+    // 错误接收及处理
+    errorHandler: (error: any, opts: any) => {
+      if (opts?.skipErrorHandler) throw error;
+      //TODO  我们的 errorThrower 抛出的错误。
+    },
+  },
   requestInterceptors: [
     // 直接写一个 function，作为拦截器
     (url, options) => {
@@ -31,7 +49,6 @@ export const request: any = {
       const { data = {} as any } = response;
       if (data.code !== SUCCESS_CODE) {
         message.warning(data.message);
-        return;
       }
       return response;
     },
