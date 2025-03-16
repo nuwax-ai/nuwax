f73bff997aefba8d360bfa154b8e0a304b522233
pujing <398996745@qq.com>
2025-03-16 20:21:27 +0800

    注释config.ts

--- config/config.ts
+++ config/config.ts
@@ -1,6 +1,6 @@
-import CopyWebpackPlugin from 'copy-webpack-plugin'; // 确保正确引入
-import MonacoWebpackPlugin from 'monaco-editor-webpack-plugin';
-import path from 'path';
+// import CopyWebpackPlugin from 'copy-webpack-plugin'; // 确保正确引入
+// import MonacoWebpackPlugin from 'monaco-editor-webpack-plugin';
+// import path from 'path';
 import { defineConfig } from 'umi';
 import routes from '../src/routes';
 
@@ -20,34 +20,34 @@ export default defineConfig({
   //   esbuildMinifyIIFE: true,
   // },
   npmClient: 'pnpm',
-  chainWebpack(config) {
-    config.externals({
-      'monaco-editor/esm/vs/editor/editor.worker': 'self.MonacoEditorWorker',
-      'monaco-editor/esm/vs/language/json/json.worker': 'self.JSONWorker',
-      'monaco-editor/esm/vs/language/typescript/ts.worker': 'self.TSWorker',
-    });
-    // 更新monaco插件配置
-    config.plugin('monaco').use(MonacoWebpackPlugin, [
-      {
-        languages: ['javascript', 'python', 'json'],
-        globalAPI: true,
-        publicPath: './', // 修改为相对路径
-      },
-    ]);
-
-    // 优化资源复制配置
-    config.plugin('copy-monaco').use(CopyWebpackPlugin, [
-      {
-        patterns: [
-          {
-            from:
-              path.dirname(require.resolve('monaco-editor/package.json')) +
-              '/min/vs',
-            to: 'monaco-editor/vs', // 直接复制到 public/monaco-editor/vs
-            force: true,
-          },
-        ],
-      },
-    ]);
-  },
+  // chainWebpack(config) {
+  //   config.externals({
+  //     'monaco-editor/esm/vs/editor/editor.worker': 'self.MonacoEditorWorker',
+  //     'monaco-editor/esm/vs/language/json/json.worker': 'self.JSONWorker',
+  //     'monaco-editor/esm/vs/language/typescript/ts.worker': 'self.TSWorker',
+  //   });
+  //   // 更新monaco插件配置
+  //   config.plugin('monaco').use(MonacoWebpackPlugin, [
+  //     {
+  //       languages: ['javascript', 'python', 'json'],
+  //       globalAPI: true,
+  //       publicPath: './', // 修改为相对路径
+  //     },
+  //   ]);
+  //
+  //   // 优化资源复制配置
+  //   config.plugin('copy-monaco').use(CopyWebpackPlugin, [
+  //     {
+  //       patterns: [
+  //         {
+  //           from:
+  //             path.dirname(require.resolve('monaco-editor/package.json')) +
+  //             '/min/vs',
+  //           to: 'monaco-editor/vs', // 直接复制到 public/monaco-editor/vs
+  //           force: true,
+  //         },
+  //       ],
+  //     },
+  //   ]);
+  // },
 });
