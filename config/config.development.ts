89ff75b092e5897c4416ba2c35afd65fd786f8f0
pujing <398996745@qq.com>
2025-03-25 14:37:59 +0800

    feat: 修改开发模式dev下的域名

--- config/config.development.ts
+++ config/config.development.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'umi';
 
 export default defineConfig({
   define: {
-    'process.env.BASE_URL': 'https://xiaocai.xspaceagi.com',
+    'process.env.BASE_URL': 'https://test-nvwa-api.xspaceagi.com',
   },
   hash: true,
 });
