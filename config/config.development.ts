58d8a8bb8f3fe29139510244e43e7d8bd3c19c51
guochao.song <soddygo@qq.com>
2025-03-25 11:08:57 +0800

    开启 hash 模式，让 build 之后的产物包含 hash 后缀

--- config/config.development.ts
+++ config/config.development.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'umi';
 export default defineConfig({
   define: {
     'process.env.BASE_URL': 'https://test-nvwa-api.xspaceagi.com',
-    hash: true,
   },
+  hash: true,
 });
