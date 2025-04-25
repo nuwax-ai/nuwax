aa94ed3fd897513fa3233f2518eea2094f4815b3
pujing <398996745@qq.com>
2025-04-25 19:53:59 +0800

    feat: 新增智能体主页相关会话更多会话

---
+++ src/pages/AgentDetails/AgentSidebar/AgentConversation/HistoryConversation/index.tsx
@@ -0,0 +1,90 @@
+import CustomPopover from '@/components/CustomPopover';
+import { apiAgentConversationDelete } from '@/services/agentConfig';
+import type { ConversationInfo } from '@/types/interfaces/conversationInfo';
+import { Empty, message, Modal } from 'antd';
+import classNames from 'classnames';
+import moment from 'moment';
+import React from 'react';
+import { history, useRequest } from 'umi';
+import styles from './index.less';
+
+const cx = classNames.bind(styles);
+
+export interface HistoryConversationProps {
+  conversationList?: ConversationInfo[];
+  isOpen?: boolean;
+  onDel: (id: number) => void;
+  onCancel?: () => void;
+}
+
+/**
+ * 历史会话弹窗
+ */
+const HistoryConversation: React.FC<HistoryConversationProps> = ({
+  conversationList,
+  isOpen,
+  onCancel,
+  onDel,
+}) => {
+  // 删除会话
+  const { run: runDel } = useRequest(apiAgentConversationDelete, {
+    manual: true,
+    debounceInterval: 500,
+    onSuccess: (_: null, params: number[]) => {
+      const conversationId = params[0];
+      onDel(conversationId);
+      message.success('删除成功');
+    },
+  });
+
+  const handleLink = (id: number) => {
+    history.push(`/home/chat/${id}`);
+  };
+
+  return (
+    <Modal
+      title={<p>历史会话</p>}
+      width={600}
+      footer={null}
+      maskClosable
+      destroyOnClose
+      open={isOpen}
+      onCancel={onCancel}
+    >
+      <div className={cx(styles.container, 'overflow-y')}>
+        {conversationList?.length ? (
+          conversationList.map((item: ConversationInfo) => (
+            <CustomPopover
+              key={item.id}
+              list={[{ label: '删除' }]}
+              onClick={() => runDel(item.id)}
+            >
+              <div
+                className={cx(
+                  'flex',
+                  'items-center',
+                  'radius-6',
+                  'cursor-pointer',
+                  'hover-box',
+                  styles.row,
+                )}
+                onClick={() => handleLink(item.id)}
+              >
+                <p className={cx('flex-1')}>{item.topic}</p>
+                <span>{moment(item.created).format('MM-DD HH:mm')}</span>
+              </div>
+            </CustomPopover>
+          ))
+        ) : (
+          <div
+            className={cx('flex', 'items-center', 'content-center', 'h-full')}
+          >
+            <Empty description="暂无数据" />
+          </div>
+        )}
+      </div>
+    </Modal>
+  );
+};
+
+export default HistoryConversation;
