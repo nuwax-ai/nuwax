a42a2f33712a5e86f69a55ff87bd3c9923ebe728
dong.luo <dong.luo@xspaceagi.com>
2025-07-09 20:04:45 +0800

    fix: 更新ChatInputHome和ChatUploadFile组件，优化文件上传状态判断逻辑，调整样式以提升用户体验；新增图片加载失败时的占位图处理。

--- src/components/ChatUploadFile/index.tsx
+++ src/components/ChatUploadFile/index.tsx
@@ -1,10 +1,11 @@
 import docImage from '@/assets/images/doc_image.jpg';
+import { IMAGE_FALLBACK } from '@/constants/images.constants';
 import type { ChatUploadFileProps } from '@/types/interfaces/agentConfig';
 import { UploadFileInfo } from '@/types/interfaces/common';
 import { formatBytes } from '@/utils/byteConverter';
 import { getProgressStatus } from '@/utils/common';
 import { CloseCircleOutlined } from '@ant-design/icons';
-import { Progress } from 'antd';
+import { Image, Progress } from 'antd';
 import classNames from 'classnames';
 import React, { useCallback } from 'react';
 import styles from './index.less';
@@ -28,11 +29,14 @@ const ChatUploadFile: React.FC<ChatUploadFileProps> = ({ files, onDel }) => {
           className={cx(styles['file-box'], 'flex', 'items-center')}
         >
           {/*如果文件是图片，则显示图片，否则显示文档默认图片*/}
-          <img
+          <Image
+            width={50}
+            height={50}
             src={
               file?.type?.includes('image/') ? file?.url : (docImage as string)
             }
-            alt=""
+            fallback={IMAGE_FALLBACK}
+            preview={false}
           />
           <div className={cx('flex-1', 'overflow-hide', styles.info)}>
             <h4 className={cx('text-ellipsis')}>{file?.name}</h4>
