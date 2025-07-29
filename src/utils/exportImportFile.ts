8c427dcdf1b756c745f6418a34e55a5d517c1202
pujing <398996745@qq.com>
2025-07-29 19:52:12 +0800

    perf: 优化代码

--- src/utils/exportImportFile.ts
+++ src/utils/exportImportFile.ts
@@ -14,10 +14,9 @@ export const exportConfigFile = async (
   type: AgentComponentTypeEnum = AgentComponentTypeEnum.Agent,
 ) => {
   try {
-    const _res = await apiTemplateExport(id, type);
-    console.log('导出配置', _res);
+    const res = await apiTemplateExport(id, type);
     // 当使用 getResponse: true 时，_res 是一个包含 data 属性的响应对象
-    const blob = new Blob([_res.data]); // 将响应数据转换为 Blob 对象
+    const blob = new Blob([res.data]); // 将响应数据转换为 Blob 对象
     const objectURL = URL.createObjectURL(blob); // 创建一个 URL 对象
     const link = document.createElement('a'); // 创建一个 a 标签
     link.href = objectURL;
@@ -35,9 +34,9 @@ export const exportConfigFile = async (
  */
 export const exportTableExcel = async (tableId: number, fileName: string) => {
   try {
-    const _res = await apiExportExcel(tableId);
+    const res = await apiExportExcel(tableId);
     // 当使用 getResponse: true 时，_res 是一个包含 data 属性的响应对象
-    const blob = new Blob([_res.data], {
+    const blob = new Blob([res.data], {
       type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
     }); // 将响应数据转换为 Blob 对象
     const objectURL = URL.createObjectURL(blob); // 创建一个 URL 对象
