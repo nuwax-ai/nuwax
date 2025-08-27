afc477ca769e75c3f45c726ea75515161242a311
pujing <398996745@qq.com>
2025-08-27 20:24:26 +0800

    perf: 生态市场卡片添加右上角提示有版本更新样式

--- src/components/EcosystemCard/NewVersionIcon.tsx
+++
@@ -1,6 +0,0 @@
-import classNames from 'classnames';
-import styles from './index.less';
-const cx = classNames.bind(styles);
-export default function NewVersionIcon() {
-  return <div className={cx(styles.newVersionIcon)}>有版本更新</div>;
-}
