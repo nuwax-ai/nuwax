6fc763656f466f7e4c7863716fd411c6ae6d75e6
dongdada29 <dongdada29@gmail.com>
2025-09-07 13:55:25 +0800

    feat(AgentSidebar): 新增删除和编辑图标，优化定时任务列表样式
    - 在定时任务列表中新增了删除和编辑图标，使用 SvgIcon 组件进行封装，提升了图标的可复用性。
    - 调整了任务项的样式，增加了间距和响应式设计，确保在不同屏幕下的适配效果。
    - 新增 delete.svg 图标文件，丰富了图标库，增强了用户操作的直观性。

--- src/components/base/SvgIcon/common.constants.tsx
+++ src/components/base/SvgIcon/common.constants.tsx
@@ -3,6 +3,8 @@ import { ReactComponent as CaretLeftSvgFile } from '@/assets/icons/common/caret_
 import { ReactComponent as CaretRightSvgFile } from '@/assets/icons/common/caret_right.svg';
 import { ReactComponent as CaretUpSvgFile } from '@/assets/icons/common/caret_up.svg';
 import { ReactComponent as DebugSvgFile } from '@/assets/icons/common/debug.svg';
+import { ReactComponent as DeleteSvgFile } from '@/assets/icons/common/delete.svg';
+import { ReactComponent as EditSvgFile } from '@/assets/icons/common/edit.svg';
 import { ReactComponent as MoreSvgFile } from '@/assets/icons/common/more.svg';
 import { ReactComponent as PlusSvgFile } from '@/assets/icons/common/plus.svg';
 import { ReactComponent as StarsSvgFile } from '@/assets/icons/common/stars.svg';
@@ -19,6 +21,8 @@ const StarsSvg = wrapSvg(StarsSvgFile);
 const DebugSvg = wrapSvg(DebugSvgFile);
 const PlusSvg = wrapSvg(PlusSvgFile);
 const StrawSvg = wrapSvg(StrawSvgFile);
+const DeleteSvg = wrapSvg(DeleteSvgFile);
+const EditSvg = wrapSvg(EditSvgFile);
 export default {
   'icons-common-caret_down': CaretDownSvg,
   'icons-common-caret_up': CaretUpSvg,
@@ -29,4 +33,6 @@ export default {
   'icons-common-debug': DebugSvg,
   'icons-common-plus': PlusSvg,
   'icons-common-straw': StrawSvg,
+  'icons-common-delete': DeleteSvg,
+  'icons-common-edit': EditSvg,
 } as Record<string, React.FC>;
