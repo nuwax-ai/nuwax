78a90348c02df49cb88e495df7c5baf6eca55edd
dongdada29 <dongdada29@gmail.com>
2025-09-09 18:26:50 +0800

    style(Square): 优化方形页面横幅样式
    - 修改方形页面的横幅样式，使用 backgroundImage 属性替代 background，提升样式一致性
    - 新增背景重复属性，确保背景图像不重复显示，优化视觉效果

--- src/pages/Square/index.tsx
+++ src/pages/Square/index.tsx
@@ -220,11 +220,9 @@ const Square: React.FC = () => {
           className={cx(styles.header)}
           onClick={handleLink}
           style={{
-            background: `url(${
+            backgroundImage: `url(${
               configInfo?.squareBanner || (squareBannerImage as string)
-            }) no-repeat center center`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center center',
+            })`,
           }}
         >
           <h3 className={cx('text-ellipsis-2')}>
