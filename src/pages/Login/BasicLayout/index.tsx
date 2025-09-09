aec76d3ae84d0014fb394ceb01c7cdd110bcdb97
xiedaokun <102251868@qq.com>
2025-09-10 04:31:58 +0800

    feat(Login): 动态展示登录页面标题和副标题
    - 注释掉了原有的静态标题和副标题
    - 新增了从 tenantConfigInfo 中获取登录页面文本和副文本的动态展示
    - 使用 dangerouslySetInnerHTML 实现 HTML 内容的动态渲染

--- src/pages/Login/BasicLayout/index.tsx
+++ src/pages/Login/BasicLayout/index.tsx
@@ -22,12 +22,25 @@ const BasicLayout: React.FC<PropsWithChildren> = ({ children }) => {
           </ConditionRender>
         </div>
         <div className={cx(styles['left-title-box'])}>
-          <p>轻松构建部署您</p>
-          <p className={cx(styles.title)}>私有的 Agentic AI 解决方案</p>
-          <p className={cx(styles['sub-title'])}>
-            提供完善的工作流、插件开发能力，RAG
-            知识库与数据表存储能力，MCP接入以及开放能力
-          </p>
+          {/*<p>轻松构建部署您</p>*/}
+          {/*<p className={cx(styles.title)}>私有的 Agentic AI 解决方案</p>*/}
+          {/*<p className={cx(styles['sub-title'])}>*/}
+          {/*  提供完善的工作流、插件开发能力，RAG*/}
+          {/*  知识库与数据表存储能力，MCP接入以及开放能力*/}
+          {/*</p>*/}
+
+          <p
+            className={cx(styles.title)}
+            dangerouslySetInnerHTML={{
+              __html: tenantConfigInfo?.loginPageText,
+            }}
+          />
+          <p
+            className={cx(styles['sub-title'])}
+            dangerouslySetInnerHTML={{
+              __html: tenantConfigInfo?.loginPageSubText,
+            }}
+          />
         </div>
       </div>
       <div className={cx(styles['right-box'])}>{children}</div>
