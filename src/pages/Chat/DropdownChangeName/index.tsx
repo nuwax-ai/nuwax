d8c2f08563d57404b67dfeb5b5d5d23fec41c472
xiedaokun <102251868@qq.com>
2025-09-15 15:25:49 +0800

    feat(Chat): 修改群聊名称或删除群聊时同步更新会话记录
    - 在 DropdownChangeName组件中使用 useModel 钩子获取 runHistory 方法
    - 修改群聊名称或删除群聊成功后，调用 runHistory 方法更新会话记录
    - 优化了用户交互体验，确保聊天页面实时反映最新的会话信息

--- src/pages/Chat/DropdownChangeName/index.tsx
+++ src/pages/Chat/DropdownChangeName/index.tsx
@@ -20,7 +20,7 @@ import {
 import { ModalFuncProps } from 'antd/es/modal/interface';
 import classNames from 'classnames';
 import React, { useState } from 'react';
-import { useNavigate } from 'umi';
+import { useModel, useNavigate } from 'umi';
 import styles from './index.less';
 
 const cx = classNames.bind(styles);
@@ -39,6 +39,7 @@ const DropdownChangeName: React.FC<Porps> = ({
   setConversationInfo,
 }) => {
   const navigate = useNavigate();
+  const { runHistory } = useModel('conversationHistory');
 
   const [modalOpenEdit, setModalOpenEdit] = useState<boolean>(false);
   const [form] = Form.useForm();
@@ -73,6 +74,10 @@ const DropdownChangeName: React.FC<Porps> = ({
           topic: result.data.topic,
         });
         message.success('修改成功');
+        // 如果是会话聊天页（chat页），同步更新会话记录
+        runHistory({
+          agentId: null,
+        });
       }
     },
   });
@@ -84,6 +89,10 @@ const DropdownChangeName: React.FC<Porps> = ({
     onSuccess: (result: RequestResponse<null>) => {
       if (result.success) {
         message.success('删除成功');
+        // 如果是会话聊天页（chat页），同步更新会话记录
+        runHistory({
+          agentId: null,
+        });
         navigate('/', { replace: true });
       }
     },
