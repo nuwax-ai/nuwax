66856684afd876ce5bdddb783df6022bdd81dd25
xiedaokun <102251868@qq.com>
2025-09-18 11:26:06 +0800

    refactor(src/layouts/HistoryConversation/index.tsx, src/layouts/InfiniteList/index.tsx, src/types/interfaces/conversationInfo.ts): 重构历史会话组件和新增无限加载列表组件
    - 移除了`HistoryConversation`组件中不必要的导入和逻辑，简化了组件结构。
    - 新增了`InfiniteList`组件，用于实现会话列表的无限滚动加载功能。- 修改了`ConversationListParams`接口中的`agentId`和`lastId`字段类型，允许它们为`null`。
    - 将删除会话、加载数据等逻辑迁移到了新的`InfiniteList`组件中，提高了代码复用性和可维护性。

--- src/layouts/HistoryConversation/index.tsx
+++ src/layouts/HistoryConversation/index.tsx
@@ -1,54 +1,26 @@
-// import CustomPopover from '@/components/CustomPopover';
-import { apiAgentConversationDelete } from '@/services/agentConfig';
-import type { ConversationInfo } from '@/types/interfaces/conversationInfo';
-import { DeleteOutlined, LoadingOutlined } from '@ant-design/icons';
-import { Empty, message, Modal } from 'antd';
-import classNames from 'classnames';
-import dayjs from 'dayjs';
-import React, { useEffect } from 'react';
-import { history, useModel, useRequest } from 'umi';
-import styles from './index.less';
-
-const cx = classNames.bind(styles);
+import { apiAgentConversationList } from '@/services/agentConfig';
+import { Modal } from 'antd';
+import React from 'react';
+import { useModel } from 'umi';
+import InfiniteList from '../InfiniteList';
 
 /**
  * 历史会话弹窗
  */
 const HistoryConversation: React.FC = () => {
   const { openHistoryModal, setOpenHistoryModal } = useModel('layout');
-  const {
-    loadingHistory,
-    setLoadingHistory,
-    runHistory,
-    conversationList,
-    setConversationList,
-  } = useModel('conversationHistory');
 
-  // 删除会话
-  const { run: runDel } = useRequest(apiAgentConversationDelete, {
-    manual: true,
-    debounceInterval: 500,
-    onSuccess: (_: null, params: number[]) => {
-      const conversationId = params[0];
-      setConversationList((list: ConversationInfo[]) =>
-        list.filter((item) => item.id !== conversationId),
-      );
-      message.success('删除成功');
-    },
-  });
-
-  useEffect(() => {
-    if (openHistoryModal) {
-      setLoadingHistory(true);
-      runHistory({
-        agentId: null,
-      });
-    }
-  }, [openHistoryModal]);
-
-  const handleLink = (id: number, agentId: number) => {
-    setOpenHistoryModal(false);
-    history.push(`/home/chat/${id}/${agentId}`);
+  const fetchApi = async (lastId: number | null, pageSize: number) => {
+    return apiAgentConversationList({
+      agentId: null,
+      lastId,
+      limit: pageSize,
+    }).then((res) => {
+      return {
+        list: res.data ?? [],
+        hasMore: res.data.length >= pageSize,
+      };
+    });
   };
 
   return (
@@ -61,55 +33,7 @@ const HistoryConversation: React.FC = () => {
       open={openHistoryModal}
       onCancel={() => setOpenHistoryModal(false)}
     >
-      <div className={cx(styles.container, 'scroll-container')}>
-        {loadingHistory ? (
-          <div
-            className={cx('flex', 'items-center', 'content-center', 'h-full')}
-          >
-            <LoadingOutlined />
-          </div>
-        ) : conversationList?.length > 0 ? (
-          <>
-            {conversationList?.map((item: ConversationInfo) => (
-              <div
-                key={item.id}
-                className={cx(
-                  'flex',
-                  'items-center',
-                  'radius-6',
-                  'hover-box',
-                  styles.row,
-                )}
-              >
-                <p
-                  className={cx('flex-1', 'text-ellipsis', 'cursor-pointer')}
-                  onClick={() => handleLink(item.id, item.agentId)}
-                >
-                  <span style={{ marginRight: 5, width: 75 }}>
-                    {dayjs(item.created).format('MM-DD HH:mm')}
-                  </span>
-                  {item.topic}
-                </p>
-                <div
-                  className={cx(styles.icon, 'cursor-pointer')}
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    runDel(item.id);
-                  }}
-                >
-                  <DeleteOutlined />
-                </div>
-              </div>
-            ))}
-          </>
-        ) : (
-          <div
-            className={cx('flex', 'items-center', 'content-center', 'h-full')}
-          >
-            <Empty description="暂无数据" />
-          </div>
-        )}
-      </div>
+      <InfiniteList height={500} pageSize={20} loadData={fetchApi} />
     </Modal>
   );
 };
