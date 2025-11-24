41cf59a60d5170a31c68fafee9f7013fbcaf7d54
dongdada29 <dongdada29@gmail.com>
2025-11-24 19:03:50 +0800

    fix: 更新SiteFooter组件，修改footer内容为技术支持信息

--- src/components/SiteFooter/index.tsx
+++ src/components/SiteFooter/index.tsx
@@ -6,17 +6,14 @@ import styles from './index.less';
 const cx = classNames.bind(styles);
 
 // 页脚
-const SiteFooter: React.FC<SiteFooterProps> = ({ text }) => {
+const SiteFooter: React.FC<SiteFooterProps> = () => {
   return (
     <footer className={cx(styles.footer)}>
       <div
         dangerouslySetInnerHTML={{
-          __html: text || '',
+          __html: '技术支持，正元智慧集团股份有限公司，型号：V1.0',
         }}
       />
-      <a href="https://nuwax.com" target="_blank" rel="noopener noreferrer">
-        Powered by nuwax
-      </a>
     </footer>
   );
 };
