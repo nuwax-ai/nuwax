bd46fd1b3b1890ecbb0dce3d7624c11b668b360e
pujing <398996745@qq.com>
2025-12-09 16:45:36 +0800

    feat: 更新文件树面板图标支持
    - 移除不再使用的 JPG 图标，新增 SQL 和默认图标，增强文件类型的可视化支持。
    - 更新 `images.constants.ts` 文件，集中管理文件类型图标，确保图标的动态渲染更加丰富。
    - 修改 `FileTreePanel` 组件中的图标渲染逻辑，支持更多文件类型的图标展示，提升用户体验。

--- src/pages/AppDev/components/FileTreePanel/images.constants.ts
+++ src/pages/AppDev/components/FileTreePanel/images.constants.ts
@@ -1,22 +1,24 @@
 import { ReactComponent as ICON_CSS } from './icons/icon_css.svg';
+import { ReactComponent as ICON_DEFAULT } from './icons/icon_default.svg';
 import { ReactComponent as ICON_HTML } from './icons/icon_html.svg';
-import { ReactComponent as ICON_JPG } from './icons/icon_jpg.svg';
 import { ReactComponent as ICON_JS } from './icons/icon_js.svg';
 import { ReactComponent as ICON_JSON } from './icons/icon_json.svg';
 import { ReactComponent as ICON_MD } from './icons/icon_md.svg';
 import { ReactComponent as ICON_PNG } from './icons/icon_png.svg';
+import { ReactComponent as ICON_SQL } from './icons/icon_sql.svg';
 import { ReactComponent as ICON_SVG } from './icons/icon_svg.svg';
 import { ReactComponent as ICON_TS } from './icons/icon_ts.svg';
 import { ReactComponent as ICON_TSX } from './icons/icon_tsx.svg';
 
 export {
   ICON_CSS,
+  ICON_DEFAULT,
   ICON_HTML,
-  ICON_JPG,
   ICON_JS,
   ICON_JSON,
   ICON_MD,
   ICON_PNG,
+  ICON_SQL,
   ICON_SVG,
   ICON_TS,
   ICON_TSX,
