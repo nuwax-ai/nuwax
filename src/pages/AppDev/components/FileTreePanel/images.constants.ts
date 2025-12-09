39adc11db2f90e60871f08bb386e163b6a4375ee
pujing <398996745@qq.com>
2025-12-09 16:31:00 +0800

    feat: 添加文件树面板图标支持
    - 新增 `images.constants.ts` 文件，集中管理文件类型图标。
    - 在 `FileTreePanel` 组件中实现文件图标的动态渲染，支持多种文件类型（如 CSS、JS、JSON、MD、TS、TSX、SVG、PNG、JPG）。
    - 更新样式文件，调整文件和文件夹名称的颜色及字体大小，提升视觉一致性。
    - 添加多个 SVG 图标文件，确保文件树面板的图标展示更加丰富和直观。

---
+++ src/pages/AppDev/components/FileTreePanel/images.constants.ts
@@ -0,0 +1,23 @@
+import { ReactComponent as ICON_CSS } from './icons/icon_css.svg';
+import { ReactComponent as ICON_HTML } from './icons/icon_html.svg';
+import { ReactComponent as ICON_JPG } from './icons/icon_jpg.svg';
+import { ReactComponent as ICON_JS } from './icons/icon_js.svg';
+import { ReactComponent as ICON_JSON } from './icons/icon_json.svg';
+import { ReactComponent as ICON_MD } from './icons/icon_md.svg';
+import { ReactComponent as ICON_PNG } from './icons/icon_png.svg';
+import { ReactComponent as ICON_SVG } from './icons/icon_svg.svg';
+import { ReactComponent as ICON_TS } from './icons/icon_ts.svg';
+import { ReactComponent as ICON_TSX } from './icons/icon_tsx.svg';
+
+export {
+  ICON_CSS,
+  ICON_HTML,
+  ICON_JPG,
+  ICON_JS,
+  ICON_JSON,
+  ICON_MD,
+  ICON_PNG,
+  ICON_SVG,
+  ICON_TS,
+  ICON_TSX,
+};
