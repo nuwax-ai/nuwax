abff0c4801d603f0996af90c67f3d70eb88ae3d5
xiedaokun <102251868@qq.com>
2025-12-10 16:28:43 +0800

    feat: 新增 SpaceLayout 组件及样式
    - 创建 `SpaceLayout` 组件，提供灵活的布局和搜索功能，支持插槽用于自定义内容。
    - 添加样式文件 `index.less`，定义组件的基本样式和布局。
    - 在 `SpaceSkillManage` 页面中集成 `SpaceLayout`，实现技能管理界面的基本结构和功能。

---
+++ src/components/SpaceLayout/index.tsx
@@ -0,0 +1,106 @@
+import { SearchOutlined } from '@ant-design/icons';
+import { Input, Space } from 'antd';
+import classNames from 'classnames';
+import { useEffect, useState } from 'react';
+import styles from './index.less';
+
+const cx = classNames.bind(styles);
+
+interface SpaceLayoutProps {
+  children: React.ReactNode;
+  leftSlot?: React.ReactNode;
+  centerSlot?: React.ReactNode;
+  rightPrefixSlot?: React.ReactNode;
+  rightSuffixSlot?: React.ReactNode;
+  inputPlaceholder?: string;
+  title?: string;
+  defaultSearchValue?: string;
+  onSearch?: (value: string) => void;
+  // 是否隐藏滚动条
+  hideScroll?: boolean;
+}
+
+const SpaceLayout: React.FC<SpaceLayoutProps> = ({
+  children,
+  leftSlot,
+  centerSlot,
+  rightPrefixSlot,
+  rightSuffixSlot,
+  inputPlaceholder,
+  title,
+  defaultSearchValue,
+  onSearch,
+  hideScroll = false,
+}) => {
+  // 搜索框
+  const [searchValue, setSearchValue] = useState<string>('');
+
+  // 搜索框变化
+  const handleSearchChange = (value: string) => {
+    setSearchValue(value);
+    onSearch?.(value);
+  };
+
+  // 搜索框清除
+  const handleSearchClear = () => {
+    setSearchValue('');
+    onSearch?.('');
+  };
+
+  // 默认搜索值
+  useEffect(() => {
+    if (defaultSearchValue) {
+      setSearchValue(defaultSearchValue);
+    }
+  }, [defaultSearchValue]);
+
+  return (
+    <div className={cx(styles.container, 'flex', 'flex-col', 'h-full')}>
+      <div
+        className={cx('flex', 'content-between')}
+        style={{ marginBottom: 5 }}
+      >
+        <div style={{ flex: 1 }}>
+          <Space>
+            <h3 className={cx(styles.title)}>{title || ''}</h3>
+            {/* 左侧区域插槽 */}
+            {leftSlot}
+          </Space>
+        </div>
+        <div>
+          {/* 中间区域插槽 */}
+          {centerSlot}
+        </div>
+        <div style={{ flex: 1, display: 'flex', justifyContent: 'flex-end' }}>
+          <Space size={10}>
+            {/* 右侧前置区域插槽 */}
+            {rightPrefixSlot}
+            <Input
+              rootClassName={cx(styles.input)}
+              placeholder={inputPlaceholder || '请输入搜索内容'}
+              value={searchValue}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                handleSearchChange(e.target.value)
+              }
+              prefix={<SearchOutlined />}
+              allowClear
+              onClear={handleSearchClear}
+            />
+            {/* 右侧后置区域插槽 */}
+            {rightSuffixSlot}
+          </Space>
+        </div>
+      </div>
+      <div
+        className={cx(
+          styles.content,
+          hideScroll ? 'scroll-container-hide' : '',
+        )}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default SpaceLayout;
