9026b1e1e6f219646aafffd5540db54d064f271e
pujing <398996745@qq.com>
2025-12-10 15:06:04 +0800

    feat: 新增文件树面板图标支持与重构
    - 新增多个文件类型的 SVG 图标（如 CSS、HTML、JS、JSON、MD、PNG、SQL、SVG、TS、TSX），增强文件树面板的可视化支持。
    - 更新 `fileTreeImages.constants.ts` 文件，集中管理文件类型图标，确保图标的动态渲染更加丰富。
    - 重构 `FileTreePanel` 组件，支持更多文件类型的图标展示，提升用户体验。
    - 移除不再使用的 `images.constants.ts` 文件，优化代码结构。
    - 新增 `AppDevFileTree` 组件，整合文件树展示与数据资源管理功能，提升组件的可复用性和维护性。

---
+++ src/utils/fileTree.tsx
@@ -0,0 +1,53 @@
+import {
+  ICON_CSS,
+  ICON_DEFAULT,
+  ICON_HTML,
+  ICON_JS,
+  ICON_JSON,
+  ICON_MD,
+  ICON_PNG,
+  ICON_SQL,
+  ICON_SVG,
+  ICON_TS,
+  ICON_TSX,
+} from '@/constants/fileTreeImages.constants';
+
+// 获取文件图标
+export const getFileIcon = (name: string) => {
+  if (name.endsWith('.ts')) {
+    return <ICON_TS />;
+  } else if (name.endsWith('.tsx') || name.endsWith('.jsx')) {
+    return <ICON_TSX />;
+  } else if (name.endsWith('.css')) {
+    return <ICON_CSS />;
+  } else if (
+    name.endsWith('.json') ||
+    name.endsWith('.yml') ||
+    name.endsWith('.yaml')
+  ) {
+    return <ICON_JSON />;
+  } else if (name.endsWith('.md')) {
+    return <ICON_MD />;
+  } else if (name.endsWith('.html') || name.endsWith('.htm')) {
+    return <ICON_HTML />;
+  } else if (name.endsWith('.js')) {
+    return <ICON_JS />;
+  } else if (
+    name.endsWith('.png') ||
+    name.endsWith('.jpg') ||
+    name.endsWith('.jpeg') ||
+    name.endsWith('.gif') ||
+    name.endsWith('.bmp') ||
+    name.endsWith('.webp') ||
+    name.endsWith('.ico') ||
+    name.endsWith('.tiff')
+  ) {
+    return <ICON_PNG />;
+  } else if (name.endsWith('.svg')) {
+    return <ICON_SVG />;
+  } else if (name.endsWith('.sql')) {
+    return <ICON_SQL />;
+  } else {
+    return <ICON_DEFAULT />;
+  }
+};
