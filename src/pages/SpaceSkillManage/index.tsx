ec547b426cd4db5e7d63808da3740bad102a0bf3
xiedaokun <102251868@qq.com>
2025-12-11 16:15:40 +0800

    feat: 完善技能管理页面功能与组件结构
    - 在 `SpaceSkillManage` 页面中重构组件，新增技能创建功能，集成 `CreateSkill` 弹窗，提升用户交互体验。
    - 更新 `MainContent` 组件，支持父组件暴露查询技能列表的方法，增强组件的灵活性。
    - 新增样式文件 `index.less`，定义 `CreateSkill` 组件的样式，确保视觉一致性。
    - 更新类型定义，新增 `SkillBaseInfo` 和 `CreateSkillProps` 接口，提升代码可读性和维护性。

--- src/pages/SpaceSkillManage/index.tsx
+++ src/pages/SpaceSkillManage/index.tsx
@@ -1,19 +1,48 @@
 import WorkspaceLayout from '@/components/WorkspaceLayout';
 import MainContent from './MainContent';
 
+import { useRef, useState } from 'react';
+import { useParams } from 'umi';
+import CreateSkill from './CreateSkill';
 import HeaderLeftSlot from './HeaderLeftSlot';
 import HeaderRightSlot from './HeaderRightSlot';
+import { MainContentRef } from './MainContent';
 
-export default function SpaceSkillManage() {
+const SpaceSkillManage: React.FC = () => {
+  const params = useParams();
+  const spaceId = Number(params.spaceId);
+
+  // 主要内容区域 ref
+  const mainContentRef = useRef<MainContentRef>(null);
+
+  // 创建技能
+  const [openCreateSkill, setOpenCreateSkill] = useState(false);
+  const handleCreateSkill = () => {
+    setOpenCreateSkill(true);
+  };
+  const handleCreateSkillConfirm = () => {
+    // 查询技能列表
+    mainContentRef.current?.exposeQueryComponentList();
+    setOpenCreateSkill(false);
+  };
   return (
     <WorkspaceLayout
       title="技能管理"
       leftSlot={<HeaderLeftSlot />}
-      rightSlot={<HeaderRightSlot />}
+      rightSlot={<HeaderRightSlot onCreate={handleCreateSkill} />}
       hideScroll={true}
     >
       {/* 主要内容区域 */}
-      <MainContent />
+      <MainContent ref={mainContentRef} />
+      {/* 创建技能弹窗 */}
+      <CreateSkill
+        spaceId={spaceId}
+        open={openCreateSkill}
+        onCancel={() => setOpenCreateSkill(false)}
+        onConfirm={handleCreateSkillConfirm}
+      />
     </WorkspaceLayout>
   );
-}
+};
+
+export default SpaceSkillManage;
