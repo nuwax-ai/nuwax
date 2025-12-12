2783a8b341dbce48611a7b4ab53bfe7b2be5c95e
xiedaokun <102251868@qq.com>
2025-12-12 17:17:28 +0800

    feat: 完善任务中心功能及相关组件
    - 在 `SpaceTaskCenter` 页面中新增任务创建、删除和详情查看功能，提升用户交互体验。
    - 新增 `HeaderLeftSlot` 和 `HeaderRightSlot` 组件，支持任务过滤和创建操作，增强页面功能性。
    - 引入 `MainContent` 组件，展示任务列表并支持点击操作，优化内容展示。
    - 新增 `CreateSkill` 组件，提供任务创建弹窗，支持技能名称、描述和图标上传功能。
    - 添加样式文件以优化组件布局，确保良好的视觉效果和用户体验。

--- src/pages/SpaceTaskCenter/index.tsx
+++ src/pages/SpaceTaskCenter/index.tsx
@@ -1,7 +1,90 @@
 import WorkspaceLayout from '@/components/WorkspaceLayout';
+import { ApplicationMoreActionEnum } from '@/types/enums/space';
+import type { CustomPopoverItem } from '@/types/interfaces/common';
+import type { ComponentInfo } from '@/types/interfaces/library';
+import { useRef, useState } from 'react';
+import { history, useParams } from 'umi';
+import CreateSkill from './CreateSkill';
+import HeaderLeftSlot from './HeaderLeftSlot';
+import HeaderRightSlot from './HeaderRightSlot';
+import MainContent, { MainContentRef } from './MainContent';
 
-const SpaceTaskCenter = () => {
-  return <WorkspaceLayout title="任务中心" hideScroll={true}></WorkspaceLayout>;
+const SpaceTaskCenter: React.FC = () => {
+  const params = useParams();
+  const spaceId = Number(params.spaceId);
+
+  // 主要内容区域 ref
+  const mainContentRef = useRef<MainContentRef>(null);
+
+  // 创建任务
+  const [openCreateTask, setOpenCreateTask] = useState(false);
+  const handleCreateTask = () => {
+    setOpenCreateTask(true);
+  };
+  const handleCreateTaskConfirm = () => {
+    // 查询任务列表
+    mainContentRef.current?.exposeQueryComponentList();
+    setOpenCreateTask(false);
+  };
+
+  // 点击任务卡片
+  const handleClickItem = (info: ComponentInfo) => {
+    const { id } = info;
+    history.push(`/space/${spaceId}/agent/${id}`);
+  };
+
+  // 删除任务
+  const handleClickDelete = (info: ComponentInfo) => {
+    console.log(info);
+  };
+
+  // 详情任务
+  const handleClickDetail = (info: ComponentInfo) => {
+    console.log(info);
+  };
+
+  // 点击任务卡片更多操作
+  const handleClickMore = (item: CustomPopoverItem, info: ComponentInfo) => {
+    const { action } = item as unknown as {
+      action: ApplicationMoreActionEnum;
+    };
+
+    switch (action) {
+      // 详情
+      case ApplicationMoreActionEnum.Detail:
+        handleClickDetail(info);
+        break;
+      // 删除
+      case ApplicationMoreActionEnum.Del:
+        handleClickDelete(info);
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <WorkspaceLayout
+      title="任务中心"
+      leftSlot={<HeaderLeftSlot />}
+      rightSlot={<HeaderRightSlot onCreate={handleCreateTask} />}
+      hideScroll={true}
+    >
+      {/* 主要内容区域 */}
+      <MainContent
+        ref={mainContentRef}
+        onClickItem={handleClickItem}
+        onClickMore={handleClickMore}
+      />
+      {/* 创建任务弹窗 */}
+      <CreateSkill
+        spaceId={spaceId}
+        open={openCreateTask}
+        onCancel={() => setOpenCreateTask(false)}
+        onConfirm={handleCreateTaskConfirm}
+      />
+    </WorkspaceLayout>
+  );
 };
 
 export default SpaceTaskCenter;
