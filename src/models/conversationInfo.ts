88a26d1df89014d595236c2a87c4bf13641330f4
pujing <398996745@qq.com>
2026-01-07 13:50:16 +0800

    feat(Conversation): 新增会话消息列表查询功能
    - 添加 apiAgentConversationMessageList 接口以支持查询会话消息列表
    - 在 conversationInfo.ts 中实现消息列表状态管理，增加 isMoreMessage 状态以控制消息加载
    - 引入 MESSAGE_LIST_SIZE 常量以限制每次查询的消息数量
    - 更新相关类型定义，确保类型安全

--- src/models/conversationInfo.ts
+++ src/models/conversationInfo.ts
@@ -7,6 +7,7 @@ import {
   apiAgentConversation,
   apiAgentConversationChatStop,
   apiAgentConversationChatSuggest,
+  apiAgentConversationMessageList,
   apiAgentConversationUpdate,
 } from '@/services/agentConfig';
 import {
@@ -72,6 +73,9 @@ import { useCallback, useRef, useState } from 'react';
 import { useModel } from 'umi';
 import { v4 as uuidv4 } from 'uuid';
 
+// 会话消息列表数量
+const MESSAGE_LIST_SIZE = 20; // 20条
+
 export default () => {
   // 历史记录
   const { runHistory, runHistoryItem } = useModel('conversationHistory');
@@ -92,6 +96,8 @@ export default () => {
   const setIsSuggest = (suggest: boolean) => {
     isSuggest.current = suggest;
   };
+  // 是否还有更多消息
+  const [isMoreMessage, setIsMoreMessage] = useState<boolean>(true);
   // 会话信息
   const [messageList, setMessageList] = useState<MessageInfo[]>([]);
   // 缓存消息列表，用于消息会话错误时，修改消息状态（将当前会话的loading状态的消息改为Error状态）
@@ -511,6 +517,32 @@ export default () => {
     handleChatProcessingList(list);
   };
 
+  // 查询会话消息列表
+  const { runAsync: runQueryConversationMessageList } = useRequest(
+    apiAgentConversationMessageList,
+    {
+      manual: true,
+      debounceWait: 300,
+      // onSuccess: (result: RequestResponse<MessageInfo[]>) => {
+      //   console.log('查询会话消息列表', result);
+      //   const { data } = result;
+      //   if (data?.length) {
+      //     setMessageList((messageList) => {
+      //       return [...messageList, ...data];
+      //     });
+
+      //     // 如果查询到的消息数量小于20，则表示没有更多消息
+      //     if (data?.length < MESSAGE_LIST_SIZE) {
+      //       setIsMoreMessage(false);
+      //     }
+      //   } else {
+      //     // 如果查询到的消息数量为0，则表示没有更多消息
+      //     setIsMoreMessage(false);
+      //   }
+      // },
+    },
+  );
+
   // 查询会话
   const {
     run: runQueryConversation,
@@ -563,6 +595,11 @@ export default () => {
           const guidQuestionDtos = data?.agent?.guidQuestionDtos || [];
           // 如果存在预置问题，显示预置问题
           setChatSuggestList(guidQuestionDtos);
+        }
+
+        // 如果消息列表数量小于20(此接口后端限制为20条)，则表示没有更多消息
+        if (len < MESSAGE_LIST_SIZE) {
+          setIsMoreMessage(false);
         }
       }
       // 不存在会话消息时，才显示开场白预置问题
@@ -1225,6 +1262,9 @@ export default () => {
     manualComponents,
     messageList,
     setMessageList,
+    runQueryConversationMessageList,
+    isMoreMessage,
+    setIsMoreMessage,
     requestId,
     finalResult,
     setFinalResult,
