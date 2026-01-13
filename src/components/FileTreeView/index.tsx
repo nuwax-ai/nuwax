9ab9c91d8d6da11efe1801b28a3dc25df5073579
pujing <398996745@qq.com>
2026-01-13 11:02:58 +0800

    fix(FileTreeView): 增强文件预览逻辑以支持文件类型判断
    - 在 FileTreeView 组件中，新增逻辑判断文件是否支持预览，优化文件选择流程。
    - 对于不支持预览的文件类型（如压缩文件）或链接文件，直接设置选中文件节点，提升用户体验和代码的健壮性。

--- src/components/FileTreeView/index.tsx
+++ src/components/FileTreeView/index.tsx
@@ -385,6 +385,14 @@ const FileTreeView = forwardRef<FileTreeViewRef, FileTreeViewProps>(
           // 其他类型文件：使用文件代理URL获取文件内容
           // "fileProxyUrl": "/api/computer/static/1464425/国际财经分析报告_20241222.md"
           else if (fileProxyUrl) {
+            // 判断文件是否支持预览（白名单方案）
+            const isPreviewable = isPreviewableFile(fileNode?.name || '');
+            // 如果文件不支持预览或文件是链接文件，则直接设置选中文件节点（如.zip、.rar、.7z 等压缩文件，不支持预览，也不需要获取压缩文件内容）
+            if (!isPreviewable || fileNode?.isLink) {
+              setSelectedFileNode(fileNode);
+              return;
+            }
+
             // 获取文件内容并更新文件树
             const newFileContent = await fetchFileContentUpdateFiles(
               fileProxyUrl,
