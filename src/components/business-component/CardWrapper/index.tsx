cda518ec4c8334185b512bac82f44adb60acec9c
xiedaokun <102251868@qq.com>
2026-03-12 10:30:26 +0800

    fix: CardWrapper 组件添加骨架屏功能

--- src/components/business-component/CardWrapper/index.tsx
+++ src/components/business-component/CardWrapper/index.tsx
@@ -1,4 +1,5 @@
 import AuthorInfo from '@/components/base/AuthorInfo';
+import { Skeleton } from 'antd';
 import classNames from 'classnames';
 import React, { PropsWithChildren } from 'react';
 import styles from './index.less';
@@ -25,6 +26,8 @@ interface CardWrapperProps {
   icon: string;
   // 默认图片
   defaultIcon: string;
+  // 加载状态
+  loading?: boolean;
 }
 
 /**
@@ -42,7 +45,90 @@ const CardWrapper: React.FC<PropsWithChildren<CardWrapperProps>> = ({
   icon,
   defaultIcon,
   style,
+  loading = false,
 }) => {
+  if (loading) {
+    return (
+      <div
+        className={cx('flex', 'flex-col', 'gap-2', styles.container, className)}
+        style={{ ...style, height: 170 }}
+      >
+        <header className={cx('flex', styles.header)} style={{ gap: 8 }}>
+          <Skeleton.Avatar
+            active
+            size="large"
+            shape="square"
+            className={styles.image}
+            style={{ width: 50, height: 50, borderRadius: 10 }}
+          />
+          <div
+            className={cx(
+              'flex-1',
+              'flex',
+              'flex-col',
+              'content-between',
+              'overflow-hide',
+            )}
+            style={{ height: 50 }}
+          >
+            <Skeleton.Input
+              active
+              size="small"
+              style={{ width: '60%', height: 18 }}
+            />
+            <div
+              className={cx('flex', 'items-center', styles['author-rel-info'])}
+              style={{ height: 16 }}
+            >
+              <Skeleton.Avatar
+                active
+                size="small"
+                style={{ width: 16, height: 16, marginRight: 4 }}
+              />
+              <Skeleton.Input
+                active
+                size="small"
+                style={{ width: 60, height: 14 }}
+              />
+              <div className={cx('ml-auto')}>
+                <Skeleton.Input
+                  active
+                  size="small"
+                  style={{ width: 80, height: 14 }}
+                />
+              </div>
+            </div>
+          </div>
+        </header>
+        <div style={{ marginTop: 4 }}>
+          <Skeleton
+            active
+            paragraph={{ rows: 1, width: ['90%'] }}
+            title={false}
+          />
+        </div>
+        <div
+          className={cx('flex', 'content-between', 'items-center')}
+          style={{ marginTop: 'auto' }}
+        >
+          <Skeleton.Button
+            active
+            size="small"
+            style={{ width: 60, height: 20 }}
+          />
+          <div className={cx('flex', 'gap-2')}>
+            <Skeleton.Button
+              active
+              size="small"
+              style={{ width: 32, height: 20 }}
+            />
+            {/* <Skeleton.Button active size="small" style={{ width: 32, height: 20 }} /> */}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div
       className={cx('flex', 'flex-col', 'gap-4', styles.container, className)}
