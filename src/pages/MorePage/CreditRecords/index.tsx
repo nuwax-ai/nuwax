78f123ccb7fdf7c50ee5f453d6fba2180062758a
xiedaokun <102251868@qq.com>
2026-06-06 11:38:47 +0800

    refactor: 使用 useLayoutEffect 进行同步滚动条检查

--- src/pages/MorePage/CreditRecords/index.tsx
+++ src/pages/MorePage/CreditRecords/index.tsx
@@ -7,7 +7,13 @@ import { apiGetCreditFlows } from '@/services/subscriptionService';
 import { type CreditRecordInfo } from '@/types/interfaces/subscription';
 import type { FormInstance, ProColumns } from '@ant-design/pro-components';
 import { Statistic, Tag } from 'antd';
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, {
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from 'react';
 import { history } from 'umi';
 import styles from './index.less';
 
@@ -102,31 +108,27 @@ const CreditRecords: React.FC<CreditRecordsProps> = ({ onClickBack }) => {
   }, [fetchData]);
 
   // 检查是否显示滚动条，若未显示且还有数据，则自动加载下一页
-  useEffect(() => {
+  useLayoutEffect(() => {
     if (loading || !hasMore || dataSource.length === 0) return;
 
     // 如果数据长度没有变，说明上一次请求未成功新增数据，不重复检查以防止死循环
     if (dataSource.length === lastCheckedLengthRef.current) return;
 
-    const timer = setTimeout(() => {
-      const tableBody = containerRef.current?.querySelector(
-        '.ant-table-body',
-      ) as HTMLDivElement;
-      if (!tableBody) return;
+    const tableBody = containerRef.current?.querySelector(
+      '.ant-table-body',
+    ) as HTMLDivElement;
+    if (!tableBody) return;
 
-      // 如果 clientHeight 为 0，说明可能容器还未分配高度或者隐藏，此时不进行检查
-      if (tableBody.clientHeight === 0) return;
+    // 如果 clientHeight 为 0，说明可能容器还未分配高度或者隐藏，此时不进行检查
+    if (tableBody.clientHeight === 0) return;
 
-      const hasScrollbar = tableBody.scrollHeight > tableBody.clientHeight;
-      if (!hasScrollbar) {
-        lastCheckedLengthRef.current = dataSource.length;
-        fetchData();
-      } else {
-        lastCheckedLengthRef.current = dataSource.length;
-      }
-    }, 100);
-
-    return () => clearTimeout(timer);
+    const hasScrollbar = tableBody.scrollHeight > tableBody.clientHeight;
+    if (!hasScrollbar) {
+      lastCheckedLengthRef.current = dataSource.length;
+      fetchData();
+    } else {
+      lastCheckedLengthRef.current = dataSource.length;
+    }
   }, [dataSource, loading, hasMore, fetchData]);
 
   const columns: ProColumns<CreditRecordInfo>[] = [
