89a4196dfa7ee66463336127c0d7cffc6ad26d0c
pujing <398996745@qq.com>
2026-09-04 20:09:46 +0800

    feat(AppDev): 新增应用域名管理接口及类型定义
    - 在 appDomain.ts 文件中新增应用域名相关的接口和类型定义，包括查询、创建、更新和删除域名的功能，增强应用管理能力。
    - 通过引入 UserAppDomainTypeEnum 和相关接口，提升代码的可读性和可维护性。

--- src/pages/AppDevPro/services/appDomain.ts
+++ src/pages/AppDevPro/services/appDomain.ts
@@ -1,14 +1,80 @@
-import { request } from "umi";
-import { RequestResponse } from "@/types/interfaces/request";
+import type { RequestResponse } from '@/types/interfaces/request';
+import { request } from 'umi';
 
-// 查询应用绑定的域名列表
+/** 应用域名类型 */
+export enum UserAppDomainTypeEnum {
+  Default = 'Default',
+  Custom = 'Custom',
+}
+
+/** 应用绑定的域名 */
+export interface UserAppDomainInfo {
+  /** 记录 ID */
+  id: number;
+  /** 商户 ID */
+  tenantId: number;
+  /** 应用 ID */
+  appId: number;
+  /** 域名 */
+  domain: string;
+  /** 域名类型 */
+  domainType: UserAppDomainTypeEnum;
+  /** 创建时间 */
+  created: string;
+  /** 更新时间 */
+  modified: string;
+}
+
+/** 绑定自有域名参数 */
+export interface UserAppDomainCreateParams {
+  /** 应用 ID */
+  appId: number;
+  /** 域名 */
+  domain: string;
+}
+
+/** 更新应用绑定的域名参数 */
+export interface UserAppDomainUpdateParams {
+  /** 记录 ID */
+  id: number;
+  /** 域名 */
+  domain: string;
+}
+
+/** 查询应用绑定的域名列表 */
 export async function apiUserAppDomainList(
   appId: number,
-): Promise<RequestResponse<any>> {
+): Promise<RequestResponse<UserAppDomainInfo[]>> {
   return request('/api/userapp/domain/list', {
     method: 'GET',
     params: {
       appId,
+    },
+  });
+}
+
+/** 绑定自有域名 */
+export async function apiUserAppDomainCreate(data: UserAppDomainCreateParams): Promise<RequestResponse<UserAppDomainInfo>> {
+  return request('/api/userapp/domain/create', {
+    method: 'POST',
+    data,
+  });
+}
+
+// 换绑域名
+export async function apiUserAppDomainUpdate(data: UserAppDomainUpdateParams): Promise<RequestResponse<UserAppDomainInfo>> {
+  return request('/api/userapp/domain/update', {
+    method: 'POST',
+    data,
+  });
+}
+
+// 解绑域名
+export async function apiUserAppDomainDelete(id: number): Promise<RequestResponse<null>> {
+  return request('/api/userapp/domain/delete', {
+    method: 'POST',
+    data: {
+      id,
     },
   });
 }
