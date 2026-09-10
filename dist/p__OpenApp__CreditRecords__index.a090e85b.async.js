0b417c693a7689d1d7c5a71c415f7fa0eb0e3be0
pujing <398996745@qq.com>
2026-09-10 18:06:50 +0800

    update 202609101805

---
+++ dist/p__OpenApp__CreditRecords__index.a090e85b.async.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunknuwax_frontend=self.webpackChunknuwax_frontend||[]).push([[7534,5450],{434429:function(d,a,n){n.r(a);var t=n(535711),_=n(205426),e=n(275271),s=n(794051),u=n(552676),r=function(){var o=(0,s.useParams)(),E=o.agentId;return(0,u.jsx)(t.default,{onClickBack:function(){s.history.push((0,_._u)("/app/".concat(E,"/my-subscriptions")))}})};a.default=r}}]);
