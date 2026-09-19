528861dee54bb7dc85d78d16dbfacacb05f97701
pujing <398996745@qq.com>
2026-09-19 16:22:55 +0800

    update 202609191621

--- dist/p__OpenApp__AppDetails__index.31a0f0b6.async.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunknuwax_frontend=self.webpackChunknuwax_frontend||[]).push([[7314],{877471:function(E,a,n){n.r(a);var t=n(633784),v=n(275271),s=n(316739),e=n(552676),r=function(){var _=(0,s.useParams)(),o=Number(_.agentId),u="/app/chat/:agentId/:id";return(0,e.jsx)(t.Z,{agentId:o,conversationUrl:u})};a.default=r}}]);
