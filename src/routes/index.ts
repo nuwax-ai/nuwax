5475640e47174685160d2d1b43bc48695e658f65
dongdada29 <dongdada29@gmail.com>
2026-09-22 16:44:28 +0800

    fix(system): hide theme configuration menu (2459)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -421,6 +421,7 @@ const routes = [
                 path: 'theme',
                 name: getRouteLabel('PC.Routes.themeConfig'),
                 component: '@/pages/SystemManagement/SystemConfig/ThemeConfig',
+                hideInMenu: true,
               },
               {
                 path: 'sandbox',
